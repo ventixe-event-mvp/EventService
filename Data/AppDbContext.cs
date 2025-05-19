de8db82b052c7446570e4af9fa4fc7a032804917
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-05-19 13:49:22 +0200

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EventService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventService.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+     public DbSet<Event> Events { get; set; } = null!;
+};
+
+    
---
+++ Models/Event.cs
@@ -0,0 +1,11 @@
+﻿namespace EventService.Models;
+
+public class Event
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public DateTime Date { get; set; }
+    public string Location { get; set; } = null!;
+
+}
